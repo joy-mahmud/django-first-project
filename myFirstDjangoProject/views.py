383886456e761298574703b11318335f6a91d93a
joy-mahmud <joymahmudiucse@gmail.com>
2024-09-08 21:56:19 +0600

    using for and if in template

--- myFirstDjangoProject/views.py
+++ myFirstDjangoProject/views.py
@@ -1,7 +1,13 @@
 from django.http import  HttpResponse
 from django.shortcuts import render
 def home(request):
-    return render(request,'index.html')
+    homeData={
+        "title":"home page",
+        "range":range(10),
+        "bodyData":"this is body data",
+        "student":[{"name":"joy","roll":101,"phone":123445},{"name":"mahmud","roll":102,"phone":6353635}]
+    }
+    return render(request,'index.html',homeData)
 def aboutUs(request):
     return HttpResponse('This is about page')
 
